a37dd8b05845c56073eb5dbb00250b8ffda3f4f9
ElSnako314 <fle@mit.edu>
2023-05-17 16:38:56 -0400

    renaming

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from sentiment_analysis import sentiment_analyzer
 app = Flask('tweet_sentiment_analyzer')
 
 @app.route('/')
-def show_predict_stock_form():
-    return render_template('searchform.html')
+def query():
+    return render_template('query.html')
 
 @app.route('/results', methods=['POST'])
 def results():
@@ -13,4 +13,4 @@ def results():
     if request.method == 'POST':
         query = form['query']
         sentiment = sentiment_analyzer(query)
-        return render_template('resultsform.html', query=query, predicted_price=sentiment)
+        return render_template('results.html', query=query, sentiment_score=sentiment)
